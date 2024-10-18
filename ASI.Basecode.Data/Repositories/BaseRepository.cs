b6fb936b569be80ab103c88ad1c88b8aa410704f
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2024-10-18 19:43:18 +0800

    User and Roles added to the database

--- ASI.Basecode.Data/Repositories/BaseRepository.cs
+++ ASI.Basecode.Data/Repositories/BaseRepository.cs
@@ -9,7 +9,7 @@ namespace Basecode.Data.Repositories
     {
         protected IUnitOfWork UnitOfWork { get; set; }
 
-        protected AsiBasecodeDBContext Context => (AsiBasecodeDBContext)UnitOfWork.Database;
+        protected SCSHelpDeskContext Context => (SCSHelpDeskContext)UnitOfWork.Database;
 
         public BaseRepository(IUnitOfWork unitOfWork)
         {
---
+++ ASI.Basecode.Data/SCSHelpDeskContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ASI.Basecode.Data.Models;
+
+namespace ASI.Basecode.Data
+{
+    public partial class SCSHelpDeskContext : DbContext
+    {
+        public SCSHelpDeskContext()
+        {
+        }
+
+        public SCSHelpDeskContext(DbContextOptions<SCSHelpDeskContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Addr=localhost\\SQLEXPRESS;database=SCSHelpDesk;Integrated Security=False;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.ToTable("Role");
+
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+                entity.Property(e => e.RoleName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
