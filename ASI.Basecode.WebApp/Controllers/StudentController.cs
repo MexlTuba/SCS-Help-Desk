4d0069ede78d5db5788dd81b8fa796b407fe1109
AlixMarian <alixmarian.gonzaga.20@usjr.edu.ph>
2024-11-04 22:12:35 +0800

    Student Dashboard Navigation

--- ASI.Basecode.WebApp/Controllers/StudentController.cs
+++ ASI.Basecode.WebApp/Controllers/StudentController.cs
@@ -8,5 +8,25 @@ namespace ASI.Basecode.WebApp.Controllers
         {
             return View();
         }
+
+        public IActionResult CreateTicket()
+        {
+            return View();
+        }
+
+        public IActionResult MyTickets()
+        {
+            return View();
+        }
+
+        public IActionResult KnowledgeBase()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
     }
 }
