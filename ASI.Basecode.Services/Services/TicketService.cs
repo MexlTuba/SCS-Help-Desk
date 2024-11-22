65cbf3ba0e2dffa7347de19376df9cab356651e0
laragrace30 <laragrace.riconalla.20@usjr.edu.ph>
2024-11-22 15:08:21 +0800

    Added the assignee feature

--- ASI.Basecode.Services/Services/TicketService.cs
+++ ASI.Basecode.Services/Services/TicketService.cs
@@ -24,12 +24,15 @@ namespace ASI.Basecode.Services
                            join priority in _context.Priorities on ticket.PriorityId equals priority.PriorityId
                            join category in _context.Categories on ticket.CategoryId equals category.CategoryId
                            join status in _context.Statuses on ticket.StatusId equals status.StatusId
+                           join user in _context.Users on ticket.AssignedTo.ToString() equals user.UserId into userJoin
+                           from assignedUser in userJoin.DefaultIfEmpty() 
                            select new TicketServiceModel
                            {
                                TicketId = ticket.TicketId,
                                CreatedBy = ticket.CreatedBy,
                                Title = ticket.Title,
-                               AssignedTo = ticket.AssignedTo,
+                               AssignedTo = ticket.AssignedTo, 
+                               AssignedToName = assignedUser != null ? assignedUser.Name : "Unassigned", 
                                PriorityType = priority.PriorityType,
                                CategoryType = category.CategoryType,
                                StatusType = status.StatusType,
@@ -39,9 +42,30 @@ namespace ASI.Basecode.Services
             return tickets;
         }
 
+
         public Ticket GetTicketById(int id)
         {
             return _context.Ticket.FirstOrDefault(t => t.TicketId == id);
         }
+
+        public int GetTicketCount()
+        {
+            return _context.Ticket.Count();
+        }
+
+        public int GetTicketCountByStatus(string statusType)
+        {
+            return _context.Ticket
+                           .Include(t => t.Status)
+                           .Count(t => t.Status.StatusType == statusType);
+        }
+
+
+        public void UpdateTicket(Ticket ticket)
+        {
+            _context.Ticket.Update(ticket);
+            _context.SaveChanges();
+        }
+
     }
 }
